a13066bf4200493fd291e4eabed59235fb496010
Senni Kaipio <skaipio@cs.helsinki.fi>
2014-01-15 15:39:12 +0200

    Projektin konffaus

---
+++ Sanapuuro/src/main/java/sanapuuro/sanapuuro/App.java
@@ -0,0 +1,13 @@
+package sanapuuro.sanapuuro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
