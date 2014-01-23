b5a4f9a3b0b48c896c87c8eb02b9e5ca321cdb3d
Senni Kaipio <skaipio@cs.helsinki.fi>
2014-01-23 17:09:52 +0200

    Muutos gridiin ja sen testeihin

--- Sanapuuro/src/main/java/sanapuuro/sanapuuro/gamelogic/Grid.java
+++ Sanapuuro/src/main/java/sanapuuro/sanapuuro/gamelogic/Grid.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sanapuuro.sanapuuro.gamelogic;
 
 import sanapuuro.sanapuuro.letters.Letter;
@@ -13,37 +12,55 @@ import sanapuuro.sanapuuro.letters.Letter;
  * @author skaipio
  */
 public class Grid {
-    public final int width, height;
-    private final Letter[][] letters;
-    
+
+    public int width, height;
+    private LetterCell[][] cells;
+
     public Grid(int width, int height) {
         this.width = width;
         this.height = height;
-        this.letters = new Letter[width][height];
+        this.cells = new LetterCell[width][height];
+        this.init();
     }
-    
-    public Letter getLetterAt(int x, int y){
-        if (!isWithinGrid(x,y)){
+
+    private void init() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < width; y++) {
+                this.cells[x][y] = new LetterCell(new Coordinate(x, y));
+            }
+        }
+    }
+
+    public void clear() {
+        for(LetterCell[] row : this.cells){
+            for(LetterCell cell : row){
+                cell.clear();
+            }
+        }
+    }
+
+    public LetterCell getCellAt(int x, int y) {
+        if (!isWithinGrid(x, y)) {
             throw new IllegalArgumentException("Given coordinates are not within grid.");
         }
-        return this.letters[x][y];
+        return this.cells[x][y];
     }
-    
-    public boolean addLetterTo(int x, int y, Letter letter){
-        if (!isWithinGrid(x,y)){
+
+    public boolean addLetterTo(int x, int y, Letter letter) {
+        if (!isWithinGrid(x, y)) {
             throw new IllegalArgumentException("Given arguments are not within grid.");
         }
-        if (this.letters[x][y] == null){
-            this.letters[x][y] = letter;
+        if (!this.cells[x][y].hasLetter()) {
+            this.cells[x][y].setLetter(letter);
             return true;
         }
         return false;
     }
-    
-    private boolean isWithinGrid(int x, int y){
+
+    private boolean isWithinGrid(int x, int y) {
         return x >= 0 && x < width && y >= 0 && y < height;
     }
-    
+
 //     public enum Size {
 //
 //        SMALL(8, 8), MEDIUM(10, 10), LARGE(12, 12);
--- Sanapuuro/src/test/java/sanapuuro/sanapuuro/gamelogic/GridTest.java
+++ Sanapuuro/src/test/java/sanapuuro/sanapuuro/gamelogic/GridTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sanapuuro.sanapuuro.gamelogic;
 
 import org.junit.After;
@@ -19,80 +18,95 @@ import sanapuuro.sanapuuro.letters.Letter;
  * @author skaipio
  */
 public class GridTest {
+
     private Grid grid;
-    
+
     public GridTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         this.grid = new Grid(8, 8);
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-     @Test
-     public void throwsExceptionWhenGettingLetterOutsideGrid() {
-         boolean thrown = false;
-         try{
-             this.grid.getLetterAt(-1, 0);
-         }catch(IllegalArgumentException e){
-             thrown = true;
-         }
-         assertTrue("x=-1 was not catched as out of bounds", thrown);
-         thrown = false;
-         try{
-             this.grid.getLetterAt(0, -1);
-         }catch(IllegalArgumentException e){
-             thrown = true;
-         }
-         assertTrue("y=-1 was not catched as out of bounds", thrown);
-         thrown = false;
-         try{
-             this.grid.getLetterAt(this.grid.width, 0);
-         }catch(IllegalArgumentException e){
-             thrown = true;
-         }
-         assertTrue("x=gridWidth was not catched as out of bounds", thrown);
-         thrown = false;
-         try{
-             this.grid.getLetterAt(0, this.grid.height);
-         }catch(IllegalArgumentException e){
-             thrown = true;
-         }
-         assertTrue("y=gridHeight was not catched as out of bounds", thrown);
-     }
-     @Test
-     public void setsAndGetsLettersAndCorrectly() {
-         boolean wasAbleToAdd = this.grid.addLetterTo(0, 0, new Letter('a', 0, 0));
-         assertTrue("result should be true when successfully adding a letter", wasAbleToAdd);
-         Letter letter = this.grid.getLetterAt(0, 0);
-         assertTrue("letter was not set into grid", letter != null);
-         assertEquals('a', letter.character);
-         wasAbleToAdd = this.grid.addLetterTo(this.grid.width-1, this.grid.height-1, new Letter('b', 0, 0));
-         assertTrue("result should be true when successfully adding a letter", wasAbleToAdd);
-         letter = this.grid.getLetterAt(this.grid.width-1, this.grid.height-1);
-         assertTrue("letter was not set into grid", letter != null);
-         assertEquals('b', letter.character);
-     }
-     @Test
-     public void doesNotChangeLettersAlreadyPlaced() {
-         this.grid.addLetterTo(1, 3, new Letter('a', 0, 0));
-         boolean wasAbleToAdd = this.grid.addLetterTo(1, 3, new Letter('b', 0, 0));
-         assertFalse("result should be false when trying to place a letter on another one", wasAbleToAdd);
-         assertEquals("should not be able to replace a letter", 'a', this.grid.getLetterAt(1, 3).character);
-     }
+    @Test
+    public void gridIsFilledWithCellsAfterCreation() {
+        boolean hasNulls = false;
+        for (int x = 0; x < this.grid.width; x++) {
+            for (int y = 0; y < this.grid.height; y++) {
+                if (this.grid.getCellAt(x, y) == null){
+                    hasNulls = true;
+                    break;
+                }
+            }
+            if (hasNulls) break;
+        }
+        assertFalse("grid shouldn't have null cells", hasNulls);
+    }
+
+    @Test
+    public void throwsExceptionWhenGettingCellOutsideGrid() {
+        boolean thrown = false;
+        try {
+            this.grid.getCellAt(-1, 0);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue("x=-1 was not catched as out of bounds", thrown);
+        thrown = false;
+        try {
+            this.grid.getCellAt(0, -1);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue("y=-1 was not catched as out of bounds", thrown);
+        thrown = false;
+        try {
+            this.grid.getCellAt(this.grid.width, 0);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue("x=gridWidth was not catched as out of bounds", thrown);
+        thrown = false;
+        try {
+            this.grid.getCellAt(0, this.grid.height);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue("y=gridHeight was not catched as out of bounds", thrown);
+    }
+
+    @Test
+    public void setsAndGetsLettersAndCorrectly() {
+        boolean wasAbleToAdd = this.grid.addLetterTo(0, 0, new Letter('a', 0, 0));
+        assertTrue("result should be true when successfully adding a letter", wasAbleToAdd);
+        Letter letter = this.grid.getCellAt(0, 0).getLetter();
+        assertTrue("letter was not set into grid", letter != null);
+        assertEquals('a', letter.character);
+        wasAbleToAdd = this.grid.addLetterTo(this.grid.width - 1, this.grid.height - 1, new Letter('b', 0, 0));
+        assertTrue("result should be true when successfully adding a letter", wasAbleToAdd);
+        letter = this.grid.getCellAt(this.grid.width - 1, this.grid.height - 1).getLetter();
+        assertTrue("letter was not set into grid", letter != null);
+        assertEquals('b', letter.character);
+    }
+
+    @Test
+    public void doesNotChangeLettersAlreadyPlaced() {
+        this.grid.addLetterTo(1, 3, new Letter('a', 0, 0));
+        boolean wasAbleToAdd = this.grid.addLetterTo(1, 3, new Letter('b', 0, 0));
+        assertFalse("result should be false when trying to place a letter on another one", wasAbleToAdd);
+        assertEquals("should not be able to replace a letter", 'a', this.grid.getCellAt(1, 3).getLetter().character);
+    }
 }
